5a70f536b2e92c355dc3368a2c05232be33fd0d3
BiboSamer <bibosamer12@gmail.com>
2025-03-15 16:32:18 +0200

    AddSwagger

--- CarApi/Program.cs
+++ CarApi/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,14 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddDbDep(builder.Configuration);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "E-Commerce API", Version = "v1" });
+});
 var app = builder.Build();
+
+
+// Add Swagger services
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "E-Commerce API v1");
+    });
 }
 
 app.UseHttpsRedirection();
